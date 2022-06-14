ba9296322c35c208f90f7bb5cfefbffb9e3afa84
Gabby Niamat <gniamat@uwo.ca>
2022-06-14 18:14:05 -0400

    removed excessive js

--- src/reducers/index.js
+++
@@ -1,7 +0,0 @@
-exports.__esModule = true;
-var study_1 = require("./study");
-var redux_1 = require("redux");
-var rootReducer = (0, redux_1.combineReducers)({
-    study: study_1["default"]
-});
-exports["default"] = rootReducer;
